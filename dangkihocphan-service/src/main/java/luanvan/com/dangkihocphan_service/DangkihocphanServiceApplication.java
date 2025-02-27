1c80d2c7354ffefee94dc9c562f431da0ec13e6f
Diem-Huong123 <huongggg299552@gmail.com>
2025-02-27 18:42:30 +0700

    Initial commit

---
+++ dangkihocphan-service/src/main/java/luanvan/com/dangkihocphan_service/DangkihocphanServiceApplication.java
@@ -0,0 +1,13 @@
+package luanvan.com.dangkihocphan_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangkihocphanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangkihocphanServiceApplication.class, args);
+	}
+
+}
---
+++ dangkihocphan-service/src/test/java/luanvan/com/dangkihocphan_service/DangkihocphanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package luanvan.com.dangkihocphan_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangkihocphanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
